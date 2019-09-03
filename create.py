b7478d38e8cceffc3199b6adc2a1645bdbe860a9
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-09-03 23:02:55 +0200

    publish to heroku

--- create.py
+++ create.py
@@ -4,8 +4,8 @@ from models import *
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///currency.db'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///currency.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db.init_app(app)
--- views.py
+++ views.py
@@ -6,20 +6,21 @@ from time import gmtime, strftime
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 from flask import session as login_session
 from wtform_fields import *
+import os
 
 
 app = Flask(__name__)
 app.secret_key="sdfdsuperfdlkngflkjnlkbgirlsdhjdrefsdfucfgfgfhhyah!!!!!dfghhm;glhjkhjl,.jk"
 app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
 
-# app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///currency.db'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///currency.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 # configure flask_login
-login = LoginManager(app)
+login = LoginManager()
 login.init_app(app)
 
 @login.user_loader
@@ -58,3 +59,10 @@ def login():
         return redirect(url_for('show_tasks'))
 
     return render_template("login.html", form=login_form)
+
+
+if __name__ == '__main__':
+    # app.secret_key = 'super_secret_key'
+    PORT = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=PORT)
