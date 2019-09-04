6725c0c41cf43c53ec50bb37d8f508ac5df3c2be
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-09-04 20:16:20 +0200

    Setup APScheduler

--- views.py
+++ views.py
@@ -161,19 +161,19 @@ if __name__ == '__main__':
     # t1= threading.Thread(target=transaction_run)
     # t1.start()
     # t1.join()
-jobstores = {
-        'default': SQLAlchemyJobStore(url=app.config['SQLALCHEMY_DATABASE_URI'])
-    }
-    executors = {
-        'default': ThreadPoolExecutor(20),
-        'processpool': ProcessPoolExecutor(5)
-    }
-    job_defaults = {
-        'coalesce': True,
-        'max_instances': 1
-        # 'replace_existing':True,
+    jobstores = {
+            'default': SQLAlchemyJobStore(url=app.config['SQLALCHEMY_DATABASE_URI'])
+        }
+        executors = {
+            'default': ThreadPoolExecutor(20),
+            'processpool': ProcessPoolExecutor(5)
+        }
+        job_defaults = {
+            'coalesce': True,
+            'max_instances': 1
+            # 'replace_existing':True,
 
-    }
+        }
 
     scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults)
 
