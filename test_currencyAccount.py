5b2288105d7419262dea89de020012f5ca7a654a
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-09-07 23:09:12 +0200

    Update unittest for currency account

--- test_currencyAccount.py
+++ test_currencyAccount.py
@@ -7,9 +7,9 @@ from views import app
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app
-        # app.config['TESTING'] = True
-        # app.login_manager.init_app(app)
-        app.config['LOGIN_DISABLED'] = True
+        app.config['TESTING'] = True
+        app.login_manager.init_app(app)
+        app.config['LOGIN_DISABLED'] = False
         self.app.config['WTF_CSRF_ENABLED'] = False
         self.app_context = self.app.app_context()
         self.app_context.push()
