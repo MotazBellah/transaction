e8ba8db2d3aff8b15ef3631a63128e6b708993b7
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-09-08 18:03:03 +0200

    Updated readme file and add comments

--- README.md
+++ README.md
@@ -22,5 +22,62 @@ and connected between them using ForeignKey, Transaction class contain the main 
 - Create Flask route to register function
 - Using wtforms, build form for registration
 - Add the new user to database after validation success
-- Create login route to accept the crenditionals of the Users
+- Create login route to accept the credentials of the Users
+- Using wtforms build a login form
 - After login success redirect the user to his main page
+
+##### Add currency account for currency
+
+- Give user the ability to create and edit the currency account
+- Using wtforms and validator and create custom validation to set account details
+
+##### Submit transaction to system
+
+- Give the user who has the currency account to submit transaction to the system
+- Using wtforms and validator and create custom validation to accept the transaction request
+- display the transaction history
+- Let the user know if the transaction success or fail
+
+##### Implement a transaction processor that can run in parallel to the backend service and processes transactions
+
+- Flask-Executor use concurrent.futures to launch parallel tasks
+- `https://flask-executor.readthedocs.io/en/latest/`
+- `https://docs.python.org/3/library/concurrent.futures.html#module-concurrent.futures`
+- Created a function that work in the background that validator each transaction request and update each transaction
+
+##### Protect all public endpoints against unauthorised access in some way.
+
+- User login_required decorator
+- checked if the current user is authenticated
+
+## Database Installation on Heroku
+
+1. Create App on Heroku.
+
+2. On your app’s “Overview” page, click the “Configure Add-ons” button.
+
+3. In the “Add-ons” section of the page, type in and select “Heroku Postgres.
+
+4. Choose the “Hobby Dev - Free” plan, which will give you access to a free PostgreSQL database that will support up to 10,000 rows of data. Click “Provision..
+
+5. Click the “Heroku Postgres :: Database” link.
+
+6. Click on “Settings”, and then “View Credentials.”. This information to hock my App to the DB
+
+7. Run `heroku run python create.py` in order to convert the SQLAlchemy class to DB table and load the data to that table.
+
+## Project Files
+
+- models.py: Contain the class representation for DB table.
+
+- create.py: File to build to DB tables
+
+- views.py: application file
+
+- wtform_fields: Contain wtforms class for each forms and validator
+
+- Procfile: To  declare the process type, in this app the type is "web" and Identify thread operation
+
+- tests Folder: contain test files
+
+- requirements.txt: Contain a list of items to be installed, use the command to install all of items `pip install -r requirements.txt`
--- wtform_fields.py
+++ wtform_fields.py
@@ -13,7 +13,6 @@ def email_exists(form, field):
 
 def invalid_credentials(form, field):
     """ Password checker """
-
     email_entered = form.email.data
     password_entered = field.data
 
@@ -24,26 +23,19 @@ def invalid_credentials(form, field):
     elif not pbkdf2_sha256.verify(password_entered, user_object.password):
         raise ValidationError("Email or password is incorrect")
 
-# custom validator for registeration form, to check if email dublicate
+# custom validator for currency form, to check if bitcoin id dublicate
 def bitcoin_id_exists(form, field):
     currency_object = Currency.query.filter_by(bitcoin_id=field.data).first()
     if currency_object:
         raise ValidationError("This bitcoin id is aleardy exists")
 
-# custom validator for registeration form, to check if email dublicate
+# custom validator for currency form, to check if ethereum id dublicate
 def ethereum_id_exists(form, field):
     currency_object = Currency.query.filter_by(ethereum_id=field.data).first()
     if currency_object:
         raise ValidationError("This ethereum id is aleardy exists")
 
-
-# custom validator for registeration form, to check if email dublicate
-def ethereum_id_exists(form, field):
-    currency_object = Currency.query.filter_by(ethereum_id=field.data).first()
-    if currency_object:
-        raise ValidationError("This ethereum id is aleardy exists")
-
-# custom validator for registeration form, to check if email dublicate
+# custom validator for currency form, to check if balance is number
 def balance_not_number(form, field):
     balance_number = field.data
     if balance_number:
@@ -52,19 +44,19 @@ def balance_not_number(form, field):
         except ValueError:
             raise ValidationError("The balance should be number")
 
-
+# custom validator for currency form, to check if balance greater than 1bln
 def balance_value_exceed(form, field):
     if field.data > 1000000:
         raise ValidationError("The value exceed the limit 1 bln")
 
 
-# # custom validator for registeration form, to check if email dublicate
+# custom validator for transaction form, to check if user exists
 def target_source(form, field):
     user_object = User.query.filter_by(id=field.data).first()
     if not user_object:
         raise ValidationError("This user is not exists, Please check the ID")
 
-
+# custom validator for transaction form, to check if user has a currency account
 def target_user_account(form, field):
     target_user = User.query.filter_by(id=field.data).first()
     if target_user:
@@ -74,7 +66,7 @@ def target_user_account(form, field):
     if not currency_account:
         raise ValidationError("This user has no currency account!")
 
-
+# custom validator for transaction form, to check the currency currency_Type
 def currency_Type_exsits(form, field):
     currency_type = field.data
     if currency_type.lower() not in  ['bitcoin', 'ethereum']:
@@ -87,41 +79,51 @@ class RegistartionForm(FlaskForm):
 
     username = StringField('username_lable',
                             validators=[InputRequired(message="Username Required"),
-                                        Length(min=4, max=25, message="Username must be between 4 and 25 charachters")])
+                                        Length(min=4,
+                                               max=25,
+                                               message="Username must be between 4 and 25 charachters")])
     email = StringField('email_lable',
-                        validators=[InputRequired(message="Email Required"), Email(message="This field requires a valid email address"), email_exists])
+                        validators=[InputRequired(message="Email Required"),
+                        Email(message='''This field requires a valid email address'''), email_exists])
     description = TextAreaField('description_lable',
-                        validators=[Length(max=1000)])
+                                validators=[Length(max=1000)])
     password = PasswordField('password_lable',
                              validators=[InputRequired(message="Password Required"),
-                                        Length(min=4, max=25, message="Password must be between 4 and 25 charachters")])
+                                        Length(min=4,
+                                               max=25,
+                                               message="Password must be between 4 and 25 charachters")])
     confirm_pswd = PasswordField('confirm_pswd_lable',
                                  validators=[InputRequired(message="Password confirmation Required"),
                                             EqualTo('password', message="Password must match!")])
-
-    submit_button = SubmitField("Create")
 
 
 class LoginForm(FlaskForm):
     """ Login Form """
 
     email = StringField('email_lable',
-                        validators=[InputRequired(message="Email Required"), Email(message="This field requires a valid email address")])
+                        validators=[InputRequired(message="Email Required"),
+                                   Email(message="This field requires a valid email address")])
     password = PasswordField('password_lable',
                              validators=[InputRequired(message="Password Required"), invalid_credentials])
-    submit_button = SubmitField("Login")
 
-###### Check this form ####
+
 class CurrencyForm(FlaskForm):
     """ Currency Form """
 
-    bitcoin_id = IntegerField('bitcoin_id', validators=[InputRequired(message="Bitcoin Wallet Id is required"), bitcoin_id_exists])
-    bitcoin_balance = FloatField('bitcoin_balance', validators=[InputRequired(message="Bitcoin Wallet balance is required"), balance_not_number, balance_value_exceed])
+    bitcoin_id = IntegerField('bitcoin_id',
+                              validators=[InputRequired(message="Bitcoin Wallet Id is required"),
+                              bitcoin_id_exists])
+    bitcoin_balance = FloatField('bitcoin_balance',
+                                 validators=[InputRequired(message="Bitcoin Wallet balance is required"),
+                                 balance_not_number, balance_value_exceed])
     ethereum_id = IntegerField('ethereum_id',
-                                 validators=[InputRequired(message="Ethereum Wallet Id is required"), ethereum_id_exists])
+                               validators=[InputRequired(message="Ethereum Wallet Id is required"),
+                               ethereum_id_exists])
     ethereum_balance = FloatField('ethereum_balance',
-                             validators=[InputRequired(message="Ethereum Wallet balance is required"), balance_not_number, balance_value_exceed])
-    max_amount = FloatField('max_amount', validators=[InputRequired(message="Max amount that is allowed per transaction is required")])
+                                  validators=[InputRequired(message="Ethereum Wallet balance is required"),
+                                  balance_not_number, balance_value_exceed])
+    max_amount = FloatField('max_amount',
+                            validators=[InputRequired(message="Max amount that is allowed per transaction is required")])
 
 
 class EditCurrencyForm(FlaskForm):
@@ -134,12 +136,14 @@ class EditCurrencyForm(FlaskForm):
     max_amount = FloatField('max_amount', validators=[Optional()])
 
 
-
-###### Check this form ####
 class TransactionForm(FlaskForm):
     """ Transaction Form """
 
-    currency_amount = FloatField('currency_amount', validators=[InputRequired(message="currency amount is required")])
-    currency_Type = StringField('currency_Type', validators=[InputRequired(message="currency type is required"), currency_Type_exsits])
+    currency_amount = FloatField('currency_amount',
+                                 validators=[InputRequired(message="currency amount is required")])
+    currency_Type = StringField('currency_Type',
+                                validators=[InputRequired(message="currency type is required"),
+                                currency_Type_exsits])
     target_user = IntegerField('target_user',
-                                 validators=[InputRequired(message="target user Id is required"), target_user_account])
+                               validators=[InputRequired(message="target user Id is required"),
+                               target_user_account])
